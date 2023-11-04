999c3bcddebc3d4d67be5966c741211adb4cc196
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-11-04 22:35:33 +0000

    Head Start

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
-# configuring server using puppet
+# Configuring server using Puppet
 
 file_line { 'disable password authentication':
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'PasswordAuthentication no',
-	replace	=> true
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true
 }
 
 file_line { 'Change Identity File':
-	path	=> '/etc/ssh/ssh_config',
-	line	=> 'IdentityFille ~/.ssh/school',
-	replace	=> true
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true
 }
